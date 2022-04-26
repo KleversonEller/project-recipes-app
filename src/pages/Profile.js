e9077f1c2ab801cd550fd76d8156090eda1eaf24
saint-lag <maiasena.gabriel@gmail.com>
2022-04-26 20:37:03 -0300

    Corrigindo erro de Lint

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { userLogout } from '../utils';
+import userLogout from '../utils';
 
 function Profile() {
   const history = useHistory();
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,5 +2,4 @@ function userLogout() {
   window.localStorage.clear();
 }
 
-// eslint-disable-next-line import/prefer-default-export
-export { userLogout };
+export default userLogout;
